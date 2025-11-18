9f5c10b014d6bc721fe6733d588fdc7e3db0fc7a
sham-404 <shamsujith4@gmail.com>
2025-11-18 22:16:05 +0530

    Refactored a bit

---
+++ particle.py
@@ -0,0 +1,86 @@
+import random, math, pygame
+
+
+class GVar:
+    WIDTH = 1200
+    HEIGHT = 550
+    FPS = 30
+
+
+class Particle:
+    def __init__(
+        self, x, y, mass=None, radius=None, velocity=None, color=None, width=2
+    ):
+        if mass is None:
+            mass = random.uniform(1, 10)
+
+        if radius is None:
+            radius = int(math.sqrt(mass) * 5)
+
+        if color is None:
+            color = (
+                random.randint(50, 255),
+                random.randint(50, 255),
+                random.randint(50, 255),
+            )
+
+        self.position = pygame.math.Vector2(x, y)
+        self.velocity = pygame.math.Vector2(1, 0).rotate_rad(
+            random.uniform(0, math.tau)
+        )
+
+        if velocity is None:
+            velocity = random.uniform(2, 6)
+
+        self.velocity *= velocity
+        self.acceleration = pygame.math.Vector2(0, 0)
+        self.mass = mass
+        self.radius = radius
+        self.color = color
+        self.width = width
+
+    def edge_collision(self):
+        if self.position.x < self.radius:
+            self.position.x = self.radius
+            self.velocity.x *= -1
+        elif self.position.x > GVar.WIDTH - self.radius:
+            self.position.x = GVar.WIDTH - self.radius
+            self.velocity.x *= -1
+
+        if self.position.y < self.radius:
+            self.position.y = self.radius
+            self.velocity.y *= -1
+        elif self.position.y > GVar.HEIGHT - self.radius:
+            self.position.y = GVar.HEIGHT - self.radius
+            self.velocity.y *= -1
+
+    def collision(self, other):
+        impact = self.position - other.position
+        dist = impact.length()
+
+        if impact.length() <= self.radius + other.radius:
+            overlap = (self.radius + other.radius) - dist
+            correction = impact.normalize() * (overlap / 2)
+            self.position += correction
+            other.position -= correction
+
+            den = impact.length_squared() * (self.mass + other.mass)
+            num = (other.velocity - self.velocity).dot(impact) * impact
+
+            self.velocity += (2 * other.mass) * num / den
+            other.velocity += (2 * self.mass) * -num / den
+
+            return True
+        return False
+
+    def update(self):
+        self.position += self.velocity
+
+    def show(self, screen):
+        pygame.draw.aacircle(
+            surface=screen,
+            color=self.color,
+            center=self.position,
+            radius=self.radius,
+            width=self.width,
+        )
--- particle_collision_simulation.py
+++ particle_collision_simulation.py
@@ -2,95 +2,13 @@
 # change the anti aliased circle (pygame.draw.aacircle()) to
 # normal circle (pygame.draw.circle()) in the show() of Particle class
 
-import pygame, random, math
-
-WIDTH = 1200
-HEIGHT = 550
-FPS = 30
-
-
-class Particle:
-    def __init__(
-        self, x, y, mass=None, radius=None, velocity=None, color=None, width=2
-    ):
-        if mass is None:
-            mass = random.uniform(1, 10)
-
-        if radius is None:
-            radius = int(math.sqrt(mass) * 5)
-
-        if color is None:
-            color = (
-                random.randint(50, 255),
-                random.randint(50, 255),
-                random.randint(50, 255),
-            )
-
-        self.position = pygame.math.Vector2(x, y)
-        self.velocity = pygame.math.Vector2(1, 0).rotate_rad(
-            random.uniform(0, math.tau)
-        )
-
-        if velocity is None:
-            velocity = random.uniform(2, 6)
-
-        self.velocity *= velocity
-        self.acceleration = pygame.math.Vector2(0, 0)
-        self.mass = mass
-        self.radius = radius
-        self.color = color
-        self.width = width
-
-    def edge_collision(self):
-        if self.position.x < self.radius:
-            self.position.x = self.radius
-            self.velocity.x *= -1
-        elif self.position.x > WIDTH - self.radius:
-            self.position.x = WIDTH - self.radius
-            self.velocity.x *= -1
-
-        if self.position.y < self.radius:
-            self.position.y = self.radius
-            self.velocity.y *= -1
-        elif self.position.y > HEIGHT - self.radius:
-            self.position.y = HEIGHT - self.radius
-            self.velocity.y *= -1
-
-    def collision(self, other):
-        impact = self.position - other.position
-        dist = impact.length()
-
-        if impact.length() <= self.radius + other.radius:
-            overlap = (self.radius + other.radius) - dist
-            correction = impact.normalize() * (overlap / 2)
-            self.position += correction
-            other.position -= correction
-
-            den = impact.length_squared() * (self.mass + other.mass)
-            num = (other.velocity - self.velocity).dot(impact) * impact
-
-            self.velocity += (2 * other.mass) * num / den
-            other.velocity += (2 * self.mass) * -num / den
-
-            return True
-        return False
-
-    def update(self):
-        self.position += self.velocity
-
-    def show(self, screen):
-        pygame.draw.aacircle(
-            surface=screen,
-            color=self.color,
-            center=self.position,
-            radius=self.radius,
-            width=self.width,
-        )
+import pygame, random
+from particle import Particle, GVar
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT + 40))
+    screen = pygame.display.set_mode((GVar.WIDTH, GVar.HEIGHT + 40))
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("Arial", 20)
     running = True
@@ -99,20 +17,20 @@ def main():
     no_of_particle = 150
 
     for i in range(no_of_particle - 75):
-         particle.append(
-             Particle(
-                 random.randint(0, WIDTH),
-                 random.randint(0, HEIGHT),
-                 mass=random.randint(11, 20),
-                 width=2,
-             )
-         )
+        particle.append(
+            Particle(
+                random.randint(0, GVar.WIDTH),
+                random.randint(0, GVar.HEIGHT),
+                mass=random.randint(11, 20),
+                width=2,
+            )
+        )
 
     for i in range(75):
         particle.append(
             Particle(
-                random.randint(0, WIDTH),
-                random.randint(0, HEIGHT),
+                random.randint(0, GVar.WIDTH),
+                random.randint(0, GVar.HEIGHT),
                 mass=random.randint(2, 4),
                 width=0,
                 velocity=6,
@@ -138,7 +56,11 @@ def main():
                     no_of_collisions += 1
 
         pygame.draw.aaline(
-            screen, (50, 50, 50), (0, HEIGHT + 1), (WIDTH + 1, HEIGHT + 1), 3
+            screen,
+            (50, 50, 50),
+            (0, GVar.HEIGHT + 1),
+            (GVar.WIDTH + 1, GVar.HEIGHT + 1),
+            3,
         )
 
         text = font.render(
@@ -146,10 +68,10 @@ def main():
             True,
             (225, 225, 225),
         )
-        screen.blit(text, (20, HEIGHT + 7))
+        screen.blit(text, (20, GVar.HEIGHT + 7))
 
         pygame.display.update()
-        clock.tick(FPS)
+        clock.tick(GVar.FPS)
 
     pygame.quit()
 
